8552fae5fc26b4314948d3f32c63abb4d556deb5
Anggi-Permana-Harianja <permanaharianja@gmail.com>
2022-04-11 15:45:53 +0700

    1D array shift

---
+++ List and Array/array_shift_1D.py
@@ -0,0 +1,29 @@
+#Link:https://www.youtube.com/watch?v=nJYFh4Dl-as
+
+def shift_array(array, shift):
+    result_array = []
+    for i in range(len(array)):
+        result_array.append(array[(i + shift) % len(array)])
+        
+    return result_array
+
+import unittest
+class TestProgram(unittest.TestCase):
+    def test_program1(self):
+        array = [1, 2, 3, 4]
+        shift = 2
+        
+        result = [3, 4, 1, 2]
+        
+        self.assertEqual(shift_array(array, shift), result)
+        
+    def test_program2(self):
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        shift = 5
+        
+        result = [6, 7, 8, 9, 10, 1, 2, 3, 4, 5]
+        
+        self.assertEqual(shift_array(array, shift), result)
+
+if __name__ == '__main__':
+    unittest.main()
