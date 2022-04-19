1489c00475d3eca22bd119df8a4ad14f92b2a561
Anggi-Permana-Harianja <permanaharianja@gmail.com>
2022-04-19 11:53:05 +0700

    add case when question

---
+++ SQL/Case_when/calculate_special_bonus.sql
@@ -0,0 +1,6 @@
+-- Link: https://leetcode.com/problems/calculate-special-bonus/
+
+SELECT employee_id,
+    CASE WHEN MOD(employee_id, 2) != 0 AND LEFT(name, 1) != M THEN salary
+         ELSE 0 END AS 'bonus'
+FROM Employees
