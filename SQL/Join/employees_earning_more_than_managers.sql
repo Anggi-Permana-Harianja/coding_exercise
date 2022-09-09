f84fc2c8246e743073b1c39a145aa81af7978425
Anggi-Permana-Harianja <permanaharianja@gmail.com>
2022-09-09 22:46:20 +0700

    add Join question and another minor fix

---
+++ SQL/Join/employees_earning_more_than_managers.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+-- Hint: Self join
+
+SELECT e.name AS 'Employee'
+FROM Employee AS e INNER JOIN Employee AS m
+    ON e.managerId = m.id
+WHERE e.salary > m.salary
